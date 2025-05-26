8ac514e6ab764cf8a051d99fd13cab8c37c524fb
patronus246 <pespatron.original@gmail.com>
2025-05-26 23:26:40 +0300

    Test passed

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,64 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+
+    private final Node[] table = new Node[10];
+    private int currentIndex = 0;
+
     @Override
     public void put(K key, V value) {
+        if (this.get(key) == null) {
+            table[currentIndex] = new Node(key, value);
+            currentIndex++;
+        }
+        else {
+            for (Node node : table) {
+                if ((node != null && node.getKey() != null && node.getKey().equals(key)) || (node != null && key == null && node.getKey() == null)) {
+                    node.setValue(value);
+                }
+            }
+        }
+
+
     }
 
     @Override
     public V get(K key) {
+        for (Node node : table) {
+            if (node != null && (node.getKey() != null && node.getKey().equals(key) || (node.getKey() == null && key == null))) {
+                return (V) node.value;
+            }
+        }
+
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (Node node : table) {
+            if (node != null) {
+                size++;
+            }
+        }
+        return size;
+    }
+
+    private static class Node <K, V> {
+        private final K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
 }
